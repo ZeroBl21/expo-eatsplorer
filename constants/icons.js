dbc32e44c5171d31e0466f562f6698652096f7eb
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-15 14:17:45 -0400

    feat(app/profile): Add profile page icons

--- constants/icons.js
+++ constants/icons.js
@@ -1,6 +1,7 @@
 import bookmark from "../assets/icons/bookmark.png";
 import home from "../assets/icons/home.png";
 import plus from "../assets/icons/plus.png";
+import plus2 from "../assets/icons/plus-2.png";
 import profile from "../assets/icons/profile.png";
 import leftArrow from "../assets/icons/left-arrow.png";
 import menu from "../assets/icons/menu.png";
@@ -12,6 +13,9 @@ import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import play from "../assets/icons/play.png";
 import fingerprint from "../assets/icons/fingerprint.png";
+import pencil from "../assets/icons/pencil.png";
+import chef from "../assets/icons/chef.png";
+import recent from "../assets/icons/recent.png";
 
 import github from "../assets/icons/github.png";
 import githubDark from "../assets/icons/github-white.png";
@@ -22,6 +26,7 @@ export default {
   bookmark,
   home,
   plus,
+  plus2,
   profile,
   leftArrow,
   menu,
@@ -31,6 +36,9 @@ export default {
   logout,
   eyeHide,
   eye,
+  pencil,
+  chef,
+  recent,
   fingerprint,
   github,
   githubDark,
