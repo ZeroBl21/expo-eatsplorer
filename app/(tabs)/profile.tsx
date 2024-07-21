eacbf75b869fcbcd2c0d2fe7594af4cd5c641e24
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-20 20:45:25 -0400

    style(app/profile): Change background color

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -8,18 +8,18 @@ import Button from '@/components/Button';
 
 export default function Profile() {
   return (
-    <SafeAreaView className="flex-1 bg-primary">
+    <SafeAreaView className="flex-1 bg-offwhite">
       <ScrollView>
         <ImageBackground className="z-20 flow-row pt-[20vh]" source={images.thumbnail}>
           <Image className="h-32 w-32 p-4 rounded-full relative top-4" source={images.profile} resizeMode="contain" />
         </ImageBackground>
 
         <View className="bg-offwhite p-4 border-b-4 border-secondary gap-4">
-          <Text className="text-xl text-secondary-dark font-medium">@ArdelisUlloa</Text>
-          <Text className="text-pretty text-xs">
-            <Text className="font-bold">10</Text> Recetas Guardadas
+          <Text className="text-xl text-secondary-dark font-inter-medium">@ArdelisUlloa</Text>
+          <Text className="text-pretty text-xs font-inter-regular">
+            <Text className="font-inter-bold">10</Text> Recetas Guardadas
           </Text>
-          <Text className="text-xs">Me gusta cocinar, pero no soy creativa ni tengo tanto dominio, asi que busco recetas increibles que me motiven a seguir aprendiendo y disfrutar de la cocina</Text>
+          <Text className="text-xs font-inter-regular">Me gusta cocinar, pero no soy creativa ni tengo tanto dominio, asi que busco recetas increibles que me motiven a seguir aprendiendo y disfrutar de la cocina</Text>
           <View className="flex-row pt-2 gap-2 justify-around">
             <Button
               title="Subir Receta"
@@ -49,9 +49,9 @@ export default function Profile() {
         </View>
         <View className="bg-offwhite h-full p-4 py-6 space-y-2">
           <Text className="font-bold">Detalles:</Text>
-          <Text className="">☺ Me gustan las bananas</Text>
-          <Text className="">😥 No consumo frutos secos</Text>
-          <Text className="">🤒 Alergica al brocoli</Text>
+          <Text className="font-inter-regular">☺ Me gustan las bananas</Text>
+          <Text className="font-inter-regular">😥 No consumo frutos secos</Text>
+          <Text className="font-inter-regular">🤒 Alergica al brocoli</Text>
           <View className="items-end p-2">
             <Image className="border-2 h-16 w-32" source={images.bread} resizeMode="contain" />
           </View>
