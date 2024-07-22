c6f826dc86286e17c0113500139d7c19fbeb6331
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-22 15:02:22 -0400

    style(app/home): Small style changes

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -18,28 +18,28 @@ export default function Home() {
       <StyledScrollView className="flex-1 px-4">
         {/* Cabecera de Bienvenida */}
         <StyledView className="mt-12 mb-8">
-          <StyledText className="text-4xl font-bold text-center text-[#354d4e]">¡Bienvenido a Eatsplorer!</StyledText>
-          <StyledText className="text-xl text-center text-[#354d4e] mt-2">Descubre recetas deliciosas y más</StyledText>
+          <StyledText className="text-4xl font-inter-bold text-center text-[#354d4e]">¡Bienvenido a Eatsplorer!</StyledText>
+          <StyledText className="text-xl font-inter-medium text-center text-[#354d4e] mt-2">Descubre recetas deliciosas y más</StyledText>
         </StyledView>
 
         {/* Imagen Redondeada */}
         <StyledView className="items-center mb-10">
-          <StyledImage source={images.bread} className="w-48 h-48 rounded-full shadow-lg" />
+          <StyledImage source={images.bread} className="w-48 h-48 rounded-full shadow-lg bg-offwhite" resizeMode='contain' />
         </StyledView>
 
         {/* Secciones */}
         <StyledView className="mb-8">
-          <StyledText className="text-2xl font-semibold text-[#354d4e] mb-4">Explora Recetas</StyledText>
+          <StyledText className="text-2xl font-inter-semibold text-[#354d4e] mb-4">Explora Recetas</StyledText>
           <StyledScrollView horizontal showsHorizontalScrollIndicator={false}>
             <StyledTouchableOpacity className="bg-white p-4 rounded-lg shadow mr-4">
               <StyledImage source={icons.chef2} className="w-16 h-16 mb-2" />
-              <StyledText className="text-lg text-center text-[#354d4e]">Recetas Nuevas</StyledText>
+              <StyledText className="text-lg font-inter-regular text-center text-[#354d4e]">Recetas Nuevas</StyledText>
             </StyledTouchableOpacity>
-            <StyledTouchableOpacity className="bg-white p-4 rounded-lg shadow mr-4">
+            <StyledTouchableOpacity className="bg-white font-inter-regular p-4 rounded-lg shadow mr-4">
               <StyledImage source={icons.recent2} className="w-16 h-16 mb-2" />
               <StyledText className="text-lg text-center text-[#354d4e]">Recetas Populares</StyledText>
             </StyledTouchableOpacity>
-            <StyledTouchableOpacity className="bg-white p-4 rounded-lg shadow">
+            <StyledTouchableOpacity className="bg-white font-inter-regular p-4 rounded-lg shadow">
               <StyledImage source={icons.bookmark2} className="w-16 h-16 mb-2" />
               <StyledText className="text-lg text-center text-[#354d4e]">Mis Favoritas</StyledText>
             </StyledTouchableOpacity>
@@ -47,11 +47,10 @@ export default function Home() {
         </StyledView>
 
         {/* Botón de Logout */}
-        <StyledTouchableOpacity onPress={handleLogout} className="bg-red-500 p-4 rounded-lg mb-8">
-          <StyledText className="text-white text-center text-lg">Log out</StyledText>
+        <StyledTouchableOpacity onPress={handleLogout} className="bg-red-500  p-4 rounded-lg mb-8">
+          <StyledText className="text-white text-center font-inter-regular text-lg">Log out</StyledText>
         </StyledTouchableOpacity>
       </StyledScrollView>
-
     </StyledView>
   );
 }
