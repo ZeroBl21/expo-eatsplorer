ce1895c60a29ffd001558ea048289436f3a52e08
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-21 21:14:48 -0400

    feat(app/shelf): Add shelf/search initial structure

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -9,11 +9,11 @@ function LoginButton({
     <TouchableOpacity
       onPress={handlePress}
       activeOpacity={0.7}
-      className={`bg-offwhite w-16 h-16 rounded-full border-2 border-gray-400 flex flex-row justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""}`}
+      className={`bg-offwhite w-14 h-14 rounded-full border-2 border-gray-400 flex flex-row justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""}`}
       disabled={isLoading}
       {...props}
     >
-      <Image className="w-14 h-14 self-center" source={icon} resizeMode="contain" />
+      <Image className="w-full h-full self-center" source={icon} resizeMode="contain" />
       <Text className="sr-only">{title}</Text>
 
       {isLoading && (
