d85b66876715057ffc4e0cb5c1a004641d52b193
raynick <raynicksil@gmail.com>
2024-08-18 18:50:34 -0400

    Toda de agregar recetas funcional y shelf tambien

--- app/(tabs)/shelf.tsx
+++ app/(tabs)/shelf.tsx
@@ -1,77 +1,227 @@
-import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, View, Text, TextInput, TouchableOpacity, Alert, FlatList } from 'react-native';
+import { useAuth } from '@/context/auth-context';
+import api from '@/api/db';
+import { styled } from 'nativewind';
 
-import FormField from '@/components/FormField';
-import { images } from '@/constants';
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledTextInput = styled(TextInput);
+const StyledTouchableOpacity = styled(TouchableOpacity);
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { router } from 'expo-router';
+const Shelf = () => {
+  const { authState } = useAuth();
+  const [pantryItems, setPantryItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [newIngredient, setNewIngredient] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [filteredIngredients, setFilteredIngredients] = useState([]);
+  const [predefinedIngredients, setPredefinedIngredients] = useState([]); // Lista de ingredientes predefinidos desde la API
+  const [isValidIngredient, setIsValidIngredient] = useState(false); // Estado para validar el ingrediente
 
-const data = [
-  { id: 1, image: images.moro, saved: true, title: 'Moro de Guandules', content: 'This is the content of card 1' },
-  { id: 2, image: images.rice, saved: false, title: 'Arroz con Pollo', content: 'This is the content of card 2' },
-  { id: 3, image: images.meat, saved: false, title: 'Carne Asada', content: 'This is the content of card 3' },
-
-  { id: 4, image: images.moro, saved: true, title: 'Moro de Guandules', content: 'This is the content of card 1' },
-  { id: 5, image: images.rice, saved: false, title: 'Arroz con Pollo', content: 'This is the content of card 2' },
-  { id: 6, image: images.meat, saved: true, title: 'Carne Asada', content: 'This is the content of card 3' },
-  // ... más elementos
-];
-
-const App = () => {
-  const [items, setItems] = useState(data)
-  const [searchTerm, setSearchTerm] = useState('');
-
-  function toggleFavorite(id: number) {
-    const newData = items.map(item => {
-      if (item.id == id) {
-        return { ...item, saved: !item.saved }
+  useEffect(() => {
+    async function fetchPantryItems() {
+      setIsLoading(true);
+      const data = await api.pantry.list(authState?.token);
+      if (data.isSuccess) {
+        setPantryItems(data.pantryItems);
+      } else {
+        console.warn(data.message || 'Error loading pantry items');
       }
-      return item
-    })
+      setIsLoading(false);
+    }
 
-    setItems(newData)
-  }
+    async function fetchIngredients() {
+      const data = await api.ingredients.list(authState?.token);
+      if (data.isSuccess) {
+        setPredefinedIngredients(data.ingredients);
+      } else {
+        Alert.alert('Error', 'No se pudieron cargar los ingredientes.');
+      }
+    }
 
-  const filteredItems = items.filter(item =>
-    item.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    if (authState?.userId) {
+      fetchPantryItems();
+      fetchIngredients();
+    }
+  }, [authState]);
+
+  const handleIngredientInputChange = (text) => {
+    setNewIngredient(text);
+    if (text.trim() !== '') {
+      const filtered = predefinedIngredients.filter(ingredient =>
+        ingredient.name.toLowerCase().startsWith(text.toLowerCase())
+      );
+      setFilteredIngredients(filtered);
+      setIsValidIngredient(filtered.some(ingredient => ingredient.name.toLowerCase() === text.toLowerCase())); // Valida si el ingrediente es exacto
+    } else {
+      setFilteredIngredients([]);
+      setIsValidIngredient(false); // No hay ingrediente válido
+    }
+  };
+
+  const handleAddItem = async () => {
+    if (!isValidIngredient || !quantity.trim()) {
+      Alert.alert('Error', 'Por favor, selecciona un ingrediente de la lista y agrega una cantidad.');
+      return;
+    }
+
+    const selectedIngredient = predefinedIngredients.find(
+      ingredient => ingredient.name.toLowerCase() === newIngredient.toLowerCase()
+    );
+
+    const newItem = { 
+      id_ingrediente: selectedIngredient.id, 
+      nombre: selectedIngredient.name, 
+      cantidad: parseInt(quantity), 
+      fecha_agregado: new Date().toISOString() 
+    };
+
+    const data = await api.pantry.addOrUpdateItem(newItem, authState?.token);
+    if (data.isSuccess) {
+      Alert.alert('Éxito', 'Ingrediente agregado/actualizado en la despensa.');
+      setPantryItems([...pantryItems, newItem]);
+      setNewIngredient('');
+      setQuantity('');
+      setFilteredIngredients([]);
+      setIsValidIngredient(false);
+    } else {
+      Alert.alert('Error', data.message || 'No se pudo agregar/actualizar el ingrediente.');
+    }
+  };
+
+  const handleUpdateQuantity = async (id_ingrediente, newQuantity) => {
+    if (newQuantity <= 0) {
+      // Eliminar el ingrediente si la cantidad llega a 0
+      const result = await api.pantry.deleteItem(authState.token, id_ingrediente);
+      if (result.isSuccess) {
+        Alert.alert('Éxito', 'Ingrediente eliminado de la despensa.');
+        // Filtrar el elemento eliminado de la lista
+        setPantryItems(pantryItems.filter(item => item.id_ingrediente !== id_ingrediente));
+      } else {
+        Alert.alert('Error', result.message || 'No se pudo eliminar el ingrediente.');
+      }
+    } else {
+      // Actualizar la cantidad si es mayor que 0
+      const result = await api.pantry.updateItemQuantity(authState.token, id_ingrediente, newQuantity);
+      if (result.isSuccess) {
+        Alert.alert('Éxito', 'Cantidad actualizada correctamente.');
+        // Actualizar la lista de la despensa
+        setPantryItems(pantryItems.map(item => 
+            item.id_ingrediente === id_ingrediente ? { ...item, cantidad: newQuantity } : item
+        ));
+      } else {
+        Alert.alert('Error', result.message || 'No se pudo actualizar la cantidad.');
+      }
+    }
+  };
+
+  const handleDeleteItem = async (id_ingrediente) => {
+    const result = await api.pantry.deleteItem(authState.token, id_ingrediente);
+    if (result.isSuccess) {
+      Alert.alert('Éxito', 'Ingrediente eliminado de la despensa.');
+      setPantryItems(pantryItems.filter(item => item.id_ingrediente !== id_ingrediente));
+    } else {
+      Alert.alert('Error', result.message || 'No se pudo eliminar el ingrediente.');
+    }
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return isNaN(date.getTime()) ? 'Fecha inválida' : date.toLocaleDateString();
+  };
 
   return (
-    <SafeAreaView className='bg-primary flex-1 py-6 px-4' >
-      <FormField
-        title=""
-        placeholder="Buscar recetas..."
-        type="text"
-        value={searchTerm}
-        handleChangeText={setSearchTerm}
-      />
-      <ScrollView className='gap-y-4 w-full mx-auto mt-1 px-2'>
-        {filteredItems.map(item => (
-          <View key={item.id} className='flex-row bg-offwhite p-4 rounded-md border border-gray-300'>
-            <View className='self-center'>
-              <Image className="rounded-md h-24 w-24" source={item.image} resizeMode="cover" />
-            </View>
+    <SafeAreaView className="bg-primary flex-1 py-6 px-4">
+      <StyledView className="mb-4">
+        <StyledText className="text-3xl font-bold text-center text-yellow-600">Mi Despensa</StyledText>
+      </StyledView>
 
-            <View className='px-2'>
-              <Text className='text-md font-inter-medium' onPress={() => router.push("/(tabs)/recipe")}>{item.title}</Text>
-              <View className='gap-2 flex-row'>
-                <Text className='text-xs text-brand'>@ArdelisUlloa</Text>
-                <Text className='text-xs'>100k Likes</Text>
-              </View>
-              <View className='flex-row'>
-                <Text className='text-xs flex-1' numberOfLines={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </Text>
-              </View>
-            </View>
-
-            <View className='ml-auto'>
-              <TabBarIcon className='p-1' size={30} name={item?.saved ? "heart" : 'heart-outline'} color={"red"} onPress={() => toggleFavorite(item.id)} />
-            </View>
-          </View>
-        ))}
+      <ScrollView className="gap-y-4 w-full mx-auto mt-2 px-2">
+        {isLoading ? (
+          <StyledText className="text-center text-lg text-yellow-600">Cargando...</StyledText>
+        ) : pantryItems.length > 0 ? (
+          pantryItems.map((item) => (
+            <StyledView key={item.id_ingrediente} className="bg-white p-4 rounded-md border border-gray-300 shadow-sm mb-4">
+              <StyledText className="text-lg font-bold text-gray-800">{item.nombre}</StyledText>
+              <StyledText className="text-sm text-gray-500">Cantidad: {item.cantidad}</StyledText>
+              <StyledText className="text-sm text-gray-500">Fecha agregado: {formatDate(item.fecha_agregado)}</StyledText>
+              
+              <StyledView className="flex-row mt-2">
+                <StyledTouchableOpacity
+                  className="bg-green-600 p-2 rounded-md mr-2"
+                  onPress={() => handleUpdateQuantity(item.id_ingrediente, item.cantidad + 1)}
+                >
+                  <StyledText className="text-white">Aumentar</StyledText>
+                </StyledTouchableOpacity>
+                <StyledTouchableOpacity
+                  className="bg-red-600 p-2 rounded-md mr-2"
+                  onPress={() => handleUpdateQuantity(item.id_ingrediente, item.cantidad > 0 ? item.cantidad - 1 : 0)}
+                >
+                  <StyledText className="text-white">Disminuir</StyledText>
+                </StyledTouchableOpacity>
+                <StyledTouchableOpacity
+                  className="bg-yellow-600 p-2 rounded-md"
+                  onPress={() => handleDeleteItem(item.id_ingrediente)}
+                >
+                  <StyledText className="text-white">Eliminar</StyledText>
+                </StyledTouchableOpacity>
+              </StyledView>
+            </StyledView>
+          ))
+        ) : (
+          <StyledText className="text-center text-lg text-gray-600">Tu despensa está vacía. ¡Empieza a agregar ingredientes!</StyledText>
+        )}
       </ScrollView>
+
+      <StyledView className="mt-4 bg-white p-4 rounded-lg shadow-md">
+        <StyledText className="text-lg font-bold text-gray-800 mb-2">Agregar Ingrediente</StyledText>
+        <StyledTextInput
+          className="border border-gray-300 p-4 mb-4 rounded-lg text-gray-800"
+          placeholder="Nombre del ingrediente"
+          placeholderTextColor="#aaa"
+          value={newIngredient}
+          onChangeText={handleIngredientInputChange}
+        />
+        {/* Mostrar sugerencias de autocompletado */}
+        {filteredIngredients.length > 0 && (
+          <StyledView className="border border-gray-300 rounded-lg max-h-40 overflow-hidden">
+            <FlatList
+              data={filteredIngredients}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  key={item.id}
+                  onPress={() => {
+                    setNewIngredient(item.name);
+                    setIsValidIngredient(true);
+                    setFilteredIngredients([]); // Oculta la lista de sugerencias
+                  }}
+                >
+                  <StyledText className="p-2 bg-gray-200 border-b border-gray-300">{item.name}</StyledText>
+                </TouchableOpacity>
+              )}
+            />
+          </StyledView>
+        )}
+        <StyledTextInput
+          className="border border-gray-300 p-4 mb-4 rounded-lg text-gray-800"
+          placeholder="Cantidad"
+          placeholderTextColor="#aaa"
+          value={quantity}
+          onChangeText={setQuantity}
+          keyboardType="numeric"
+        />
+        <StyledTouchableOpacity
+          onPress={handleAddItem}
+          className={`p-4 rounded-lg ${isValidIngredient && quantity.trim() ? 'bg-yellow-600' : 'bg-gray-400'}`}
+          disabled={!isValidIngredient || !quantity.trim()}
+        >
+          <StyledText className="text-white text-center text-lg font-semibold">Agregar a Despensa</StyledText>
+        </StyledTouchableOpacity>
+      </StyledView>
     </SafeAreaView>
   );
 };
 
-export default App;
+export default Shelf;
--- app/(tabs)/upload-recipe.tsx
+++ app/(tabs)/upload-recipe.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, Alert, KeyboardAvoidingView, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { styled } from 'nativewind';
 import api from '../../api/db';
 import { images } from '../../constants';
 import { useAuth } from '../../context/auth-context';
+
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -12,135 +14,274 @@ const StyledTouchableOpacity = styled(TouchableOpacity);
 const StyledImage = styled(Image);
 const StyledScrollView = styled(ScrollView);
 
+
 export default function UploadRecipe() {
-  const { authState } = useAuth();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [instructions, setInstructions] = useState('');
-  const [ingredients, setIngredients] = useState([]);
-  const [ingredientInput, setIngredientInput] = useState('');
+ const { authState } = useAuth();
+ const [title, setTitle] = useState('');
+ const [Porciones, setPorciones] = useState('');
+ const [description, setDescription] = useState('');
+ const [instructions, setInstructions] = useState('');
+ const [ingredients, setIngredients] = useState([]);  // Lista de ingredientes seleccionados
+ const [ingredientInput, setIngredientInput] = useState('');
+ const [filteredIngredients, setFilteredIngredients] = useState([]);
+ const [predefinedIngredients, setPredefinedIngredients] = useState([]); // Lista de ingredientes predefinidos desde la API
+ const [isValidIngredient, setIsValidIngredient] = useState(false); // Estado para validar el ingrediente
+ const [image, setImage] = useState(null);  // Estado para la imagen
 
-  const addIngredient = () => {
-    if (ingredientInput.trim() !== '') {
-      setIngredients([...ingredients, ingredientInput]);
-      setIngredientInput('');
-    }
-  };
 
-  const removeIngredient = (index) => {
-    const newIngredients = [...ingredients];
-    newIngredients.splice(index, 1);
-    setIngredients(newIngredients);
-  };
+ // Fetch the list of predefined ingredients dynamically
+ useEffect(() => {
+   async function fetchIngredients() {
+     const data = await api.ingredients.list(authState?.token);
+     if (data.isSuccess) {
+       setPredefinedIngredients(data.ingredients);
+     } else {
+       Alert.alert('Error', 'No se pudieron cargar los ingredientes.');
+     }
+   }
 
-  const submitRecipe = async () => {
-    if (!authState.userId || !authState.token) {
-      Alert.alert('Error', 'User not authenticated');
-      return;
-    }
 
-    const recipe = {
-      title,
-      description,
-      instructions,
-      photo: "ipzum lorem",  // Valor fijo para la imagen
-      userId: authState.userId,
-    };
+   if (authState?.userId) {
+     fetchIngredients();
+   }
+ }, [authState]);
 
-    const recipeResponse = await api.recipes.uploadRecipe(recipe, authState.token);
+ const handleIngredientInputChange = (text) => {
+   setIngredientInput(text);
+   if (text.trim() !== '') {
+     const filtered = predefinedIngredients.filter(ingredient =>
+       ingredient.name.toLowerCase().startsWith(text.toLowerCase())
+     );
+     setFilteredIngredients(filtered);
+     setIsValidIngredient(filtered.some(ingredient => ingredient.name.toLowerCase() === text.toLowerCase())); // Valida si el ingrediente es exacto
+   } else {
+     setFilteredIngredients([]);
+     setIsValidIngredient(false); // No hay ingrediente válido
+   }
+ };
 
-    if (recipeResponse.isSuccess) {
-      const recipeId = recipeResponse.data.id_receta;
-      let ingredientIds = [];
 
-      for (let i = 0; i < ingredients.length; i++) {
-        const ingredient = { id_ingrediente: 0, nombre: ingredients[i] };
-        const ingredientResponse = await api.recipes.uploadIngredient(ingredient, authState.token);
-        
-        if (ingredientResponse.isSuccess) {
-          ingredientIds.push(ingredientResponse.data.id_ingrediente);
-        } else {
-          Alert.alert('Error', `Error uploading ingredient: ${ingredients[i]}`);
-          return;
-        }
-      }
+ const addIngredient = () => {
+   if (isValidIngredient && !ingredients.includes(ingredientInput)) {
+     setIngredients([...ingredients, ingredientInput]);
+     setIngredientInput('');
+     setFilteredIngredients([]);
+     setIsValidIngredient(false); // Reinicia la validación
+   } else {
+     Alert.alert('Error', 'El ingrediente debe seleccionarse de la lista de autocompletado y no debe estar ya en la lista.');
+   }
+ };
 
-      const relationResponse = await api.recipes.relateIngredientsToRecipe(recipeId, ingredientIds, authState.token);
 
-      if (relationResponse.isSuccess) {
-        Alert.alert('Éxito', 'Receta y ingredientes subidos exitosamente');
-      } else {
-        Alert.alert('Error', 'Hubo un problema al relacionar los ingredientes con la receta');
-      }
-    } else {
-      Alert.alert('Error', 'Hubo un problema al subir la receta');
-    }
-  };
+ const removeIngredient = (index) => {
+   const newIngredients = [...ingredients];
+   newIngredients.splice(index, 1);
+   setIngredients(newIngredients);
+ };
 
-  return (
-    <KeyboardAvoidingView 
-      behavior={Platform.OS === "ios" ? "padding" : "height"} 
-      style={{ flex: 1 }}
-    >
-      <StyledScrollView className="bg-white h-full p-4">
-        <StyledView className="items-center mt-12 mb-6">
-          <StyledImage source={images.bread} className="w-24 h-24 mb-4 rounded-full" />
-          <StyledText className="text-3xl font-bold text-yellow-600 mb-4">Subir Receta</StyledText>
-        </StyledView>
-        <StyledTextInput
-          className="border border-gray-300 p-4 mb-4 rounded-lg"
-          placeholder="Título"
-          placeholderTextColor="#888"
-          value={title}
-          onChangeText={setTitle}
-        />
-        <StyledTextInput
-          className="border border-gray-300 p-4 mb-4 rounded-lg h-40"
-          placeholder="Descripción"
-          placeholderTextColor="#888"
-          value={description}
-          onChangeText={setDescription}
-          multiline
-          numberOfLines={5}
-        />
-        <StyledTextInput
-          className="border border-gray-300 p-4 mb-4 rounded-lg h-40"
-          placeholder="Instrucciones"
-          placeholderTextColor="#888"
-          value={instructions}
-          onChangeText={setInstructions}
-          multiline
-          numberOfLines={5}
-        />
-        <StyledView className="flex flex-row mb-4">
-          <StyledTextInput
-            className="border border-gray-300 p-4 rounded-lg flex-1"
-            placeholder="Ingrediente"
-            placeholderTextColor="#888"
-            value={ingredientInput}
-            onChangeText={setIngredientInput}
-          />
-          <StyledTouchableOpacity onPress={addIngredient} className="bg-yellow-500 p-4 ml-2 rounded-lg">
-            <StyledText className="text-white">Añadir</StyledText>
-          </StyledTouchableOpacity>
-        </StyledView>
-        {ingredients.length > 0 && (
-          <StyledView className="mb-4">
-            <StyledText className="text-xl font-bold mb-2">Ingredientes:</StyledText>
-            {ingredients.map((ingredient, index) => (
-              <StyledView key={index} className="flex flex-row justify-between items-center mb-2">
-                <StyledText className="text-base">{index + 1}. {ingredient}</StyledText>
-                <StyledTouchableOpacity onPress={() => removeIngredient(index)}>
-                  <StyledText className="text-red-500">Eliminar</StyledText>
-                </StyledTouchableOpacity>
-              </StyledView>
-            ))}
-          </StyledView>
-        )}
-        <StyledTouchableOpacity onPress={submitRecipe} className="bg-yellow-600 p-4 rounded-lg">
-          <StyledText className="text-white text-center">Subir Receta</StyledText>
-        </StyledTouchableOpacity>
-      </StyledScrollView>
-    </KeyboardAvoidingView>
-  );
+
+ const pickImage = async () => {
+   let result = await ImagePicker.requestMediaLibraryPermissionsAsync();
+   if (result.status !== 'granted') {
+     Alert.alert('Permiso denegado', 'Necesitamos permisos para acceder a tus fotos.');
+     return;
+   }
+
+
+   let pickerResult = await ImagePicker.launchImageLibraryAsync({
+     mediaTypes: ImagePicker.MediaTypeOptions.Images,
+     allowsEditing: true,
+     aspect: [4, 3],
+     quality: 1,
+   });
+
+
+   if (!pickerResult.canceled) {
+     setImage(pickerResult.assets[0].uri);  // Guarda la URI de la imagen seleccionada
+   }
+ };
+
+
+ const submitRecipe = async () => {
+   if (!authState.userId || !authState.token) {
+     Alert.alert('Error', 'Usuario no autenticado');
+     return;
+   }
+
+
+   let imageUrl = null;
+
+
+   if (image) {
+     // Subir la imagen y obtener la URL
+     const imageResponse = await api.recipes.uploadImage(image, authState.token);
+     if (imageResponse.isSuccess) {
+       imageUrl = imageResponse.url;
+     } else {
+       Alert.alert('Error', 'Hubo un problema al subir la imagen');
+       return;
+     }
+   }
+
+
+   const recipe = {
+     title,
+     description,
+     instructions,
+     photo: imageUrl,  // Usamos la URL de la imagen subida
+     userId: authState.userId,
+     fecha_creacion: new Date().toISOString(),  // Fecha actual
+     porciones: Porciones,
+     likes: 0,
+   };
+
+
+   const recipeResponse = await api.recipes.uploadRecipe(recipe, authState.token);
+
+
+   if (recipeResponse.isSuccess) {
+     const recipeId = recipeResponse.data.id_receta;
+     let ingredientIds = [];
+
+
+     for (let i = 0; i < ingredients.length; i++) {
+       const ingredient = { id_ingrediente: 0, nombre: ingredients[i], medida: 'al gusto', dias_vencimiento: 0 };
+       const ingredientResponse = await api.recipes.uploadIngredient(ingredient, authState.token);
+       ingredientIds.push(ingredientResponse.data.ingrediente.id_ingrediente);
+
+
+     }
+
+
+     console.log("ID de receta: ", recipeId);
+     console.log("ID de los ingredientes: ", ingredientIds);
+
+
+     const relationResponse = await api.recipes.relateIngredientsToRecipe(recipeId, ingredientIds, authState.token);
+
+
+     if (relationResponse.isSuccess) {
+       Alert.alert('Éxito', 'Receta y ingredientes subidos exitosamente');
+     } else {
+       Alert.alert('Error', 'Hubo un problema al relacionar los ingredientes con la receta');
+     }
+   } else {
+     Alert.alert('Error', 'Hubo un problema al subir la receta');
+   }
+ };
+
+
+ return (
+   <KeyboardAvoidingView
+     behavior={Platform.OS === "ios" ? "padding" : "height"}
+     style={{ flex: 1 }}
+   >
+     <StyledScrollView className="bg-white h-full p-4">
+       <StyledView className="items-center mt-12 mb-6">
+         <StyledImage source={images.bread} className="w-24 h-24 mb-4 rounded-full" />
+         <StyledText className="text-3xl font-bold text-yellow-600 mb-4">Subir Receta</StyledText>
+       </StyledView>
+
+
+       <StyledTextInput
+         className="border border-gray-300 p-4 mb-4 rounded-lg"
+         placeholder="Título"
+         placeholderTextColor="#888"
+         value={title}
+         onChangeText={setTitle}
+       />
+       <StyledTextInput
+         className="border border-gray-300 p-4 mb-4 rounded-lg"
+         placeholder="Porciones"
+         placeholderTextColor="#888"
+         value={Porciones}
+         onChangeText={setPorciones}
+       />
+       <StyledTextInput
+         className="border border-gray-300 p-4 mb-4 rounded-lg h-40"
+         placeholder="Descripción"
+         placeholderTextColor="#888"
+         value={description}
+         onChangeText={setDescription}
+         multiline
+         numberOfLines={5}
+       />
+       <StyledTextInput
+         className="border border-gray-300 p-4 mb-4 rounded-lg h-40"
+         placeholder="Instrucciones"
+         placeholderTextColor="#888"
+         value={instructions}
+         onChangeText={setInstructions}
+         multiline
+         numberOfLines={5}
+       />
+
+
+       <StyledView className="mb-4">
+         <StyledTextInput
+           className="border border-gray-300 p-4 rounded-lg"
+           placeholder="Ingrediente"
+           placeholderTextColor="#888"
+           value={ingredientInput}
+           onChangeText={handleIngredientInputChange}
+         />
+         {/* Mostrar sugerencias de autocompletado */}
+         {filteredIngredients.length > 0 && (
+           <StyledView className="border border-gray-300 rounded-lg max-h-40 overflow-hidden">
+             {filteredIngredients.map((item) => (
+               <TouchableOpacity key={item.id} onPress={() => {
+                 setIngredientInput(item.name);
+                 setIsValidIngredient(true); // Asegura que se active el botón
+               }}>
+                 <StyledText className="p-2 bg-gray-200 border-b border-gray-300">{item.name}</StyledText>
+               </TouchableOpacity>
+             ))}
+           </StyledView>
+         )}
+       </StyledView>
+
+
+       {ingredients.length > 0 && (
+         <StyledView className="mb-4">
+           <StyledText className="text-xl font-bold mb-2">Ingredientes:</StyledText>
+           {ingredients.map((ingredient, index) => (
+             <StyledView key={index} className="flex flex-row justify-between items-center mb-2">
+               <StyledText className="text-base">{index + 1}. {ingredient}</StyledText>
+               <StyledTouchableOpacity onPress={() => removeIngredient(index)}>
+                 <StyledText className="text-red-500">Eliminar</StyledText>
+               </StyledTouchableOpacity>
+             </StyledView>
+           ))}
+         </StyledView>
+       )}
+
+
+       <StyledTouchableOpacity
+         onPress={addIngredient}
+         className={`p-4 mb-4 rounded-lg ${isValidIngredient ? 'bg-yellow-600' : 'bg-gray-300'}`}
+         disabled={!isValidIngredient} // Desactiva el botón si no es un ingrediente válido
+       >
+         <StyledText className="text-white text-center">Añadir Ingrediente</StyledText>
+       </StyledTouchableOpacity>
+
+
+       {/* Botón para elegir la imagen */}
+       <StyledTouchableOpacity onPress={pickImage} className="bg-yellow-600 p-4 mb-4 rounded-lg">
+         <StyledText className="text-white text-center">Seleccionar Foto de la Receta</StyledText>
+       </StyledTouchableOpacity>
+
+
+       {/* Mostrar imagen seleccionada */}
+       {image && (
+         <StyledImage source={{ uri: image }} className="w-full h-64 mb-4 rounded-lg" />
+       )}
+
+
+       {/* Botón para subir la receta */}
+       <StyledTouchableOpacity onPress={submitRecipe} className="bg-yellow-600 p-4 rounded-lg">
+         <StyledText className="text-white text-center">Subir Receta</StyledText>
+       </StyledTouchableOpacity>
+     </StyledScrollView>
+   </KeyboardAvoidingView>
+ );
 }
