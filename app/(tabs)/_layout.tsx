6e1dcd8bce360071a52a6dc913d4a7f5cb33b749
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-18 23:38:57 -0400

    fix: Removing recipe

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,87 +1,94 @@
-import React from 'react';
-import { Tabs, Redirect } from 'expo-router';
+import React from "react";
+import { Tabs, Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { useAuth } from '../../context/auth-context';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useAuth } from "../../context/auth-context";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const { authState, request } = useAuth();
+	const colorScheme = useColorScheme();
+	const { authState, request } = useAuth();
 
-  if (!authState?.token && request) return <Redirect href="/login" />;
+	if (!authState?.token && request) return <Redirect href="/login" />;
 
-  return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          headerShown: false,
-        }}>
-        <Tabs.Screen
-          name="home"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="search"
-          options={{
-            title: 'Search',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="shelf"
-          options={{
-            title: 'Shelf',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'menu' : 'menu-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: 'Profile',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="upload-recipe"
-          options={{
-            href: null,
-          }}
-        />
-        <Tabs.Screen
-          name="recipe"
-          options={{
-            href: null,
-          }}
-        />
-        <Tabs.Screen
-          name="edit-profile"
-          options={{
-            href: null,
-          }}
-        />
-        <Tabs.Screen
-          name="change-password"
-          options={{
-            href: null,
-          }}
-        />
-      </Tabs>
-      <StatusBar backgroundColor="#161622" style="light" />
-    </>
-  );
+	return (
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+					headerShown: false,
+				}}
+			>
+				<Tabs.Screen
+					name="home"
+					options={{
+						title: "Home",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "home" : "home-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="search"
+					options={{
+						title: "Search",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "search" : "search-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="shelf"
+					options={{
+						title: "Shelf",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "menu" : "menu-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="profile"
+					options={{
+						title: "Profile",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "person" : "person-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="upload-recipe"
+					options={{
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
+					name="edit-profile"
+					options={{
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
+					name="change-password"
+					options={{
+						href: null,
+					}}
+				/>
+			</Tabs>
+			<StatusBar backgroundColor="#161622" style="light" />
+		</>
+	);
 }
